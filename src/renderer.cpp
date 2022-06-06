e4bbcfc71eb7d2093bd252f1359949795e30602b
Quinn Painter <quinn@geekymonkey.com>
2022-06-06 19:58:09 +0100

    fixes

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -3,7 +3,7 @@
 
 screenTri screenTris[] = {{{200, 30, 0}, {600, 600, 0}, {30, 400, 0}}};
 
-inline void setPixel(u8* screenData, int x, int y, u8 r, u8 g, u8 b) {
+void setPixel(u8* screenData, int x, int y, u8 r, u8 g, u8 b) {
     if (x > 0 && x < XResolution && y > 0 && y < YResolution) {
         screenData[(y * XResolution * 3) + (x * 3)] = r;
         screenData[(y * XResolution * 3) + (x * 3) + 1] = g;
@@ -11,11 +11,23 @@ inline void setPixel(u8* screenData, int x, int y, u8 r, u8 g, u8 b) {
     }
 }
 
+bool xbounce = false;
+bool ybounce = false;
 void renderFrame(u8* screenData) {
-    screenTris[0].p3.x++;
-    if (screenTris[0].p3.x > XResolution) {screenTris[0].p3.x = 0;}
-    screenTris[0].p1.y++;
-    if (screenTris[0].p1.y > YResolution) {screenTris[0].p1.y = 0;}
+    if (xbounce) {
+        screenTris[0].p3.x++;
+        if (screenTris[0].p3.x > XResolution) {xbounce = !xbounce;}
+    } else {
+        screenTris[0].p3.x--;
+        if (screenTris[0].p3.x <= 0) {xbounce = !xbounce;}
+    }
+    if (ybounce) {
+        screenTris[0].p1.y++;
+        if (screenTris[0].p1.y > YResolution) {ybounce = !ybounce;}
+    } else {
+        screenTris[0].p1.y--;
+        if (screenTris[0].p1.y <= 0) {ybounce = !ybounce;}
+    }
 
     for (screenTri tri : screenTris) {
         setPixel(screenData, tri.p1.x, tri.p1.y, 0xFF, 0xFF, 0);
@@ -34,8 +46,6 @@ void renderFrame(u8* screenData) {
             float line2Slope = (float)(tri.p2.x - tri.p1.x) / (tri.p2.y - tri.p1.y);
             float line2X = tri.p1.x;
             for (int y = tri.p1.y; y <= tri.p2.y; y++) {
-                line1X += line1Slope;
-                line2X += line2Slope;
                 if (line1X < line2X) {
                     for (int x = line1X; x < line2X; x++) {
                         setPixel(screenData, x, y, 0x7F, 0xFF, 0);
@@ -45,6 +55,8 @@ void renderFrame(u8* screenData) {
                         setPixel(screenData, x, y, 0x7F, 0xFF, 0);
                     }
                 }
+                line1X += line1Slope;
+                line2X += line2Slope;
             }
         }
 
@@ -52,10 +64,6 @@ void renderFrame(u8* screenData) {
             float line3Slope = (float)(tri.p3.x - tri.p2.x) / (tri.p3.y - tri.p2.y);
             float line3X = tri.p2.x;
             for (int y = tri.p2.y; y <= tri.p3.y; y++) {
-                line1X += line1Slope;
-                line3X += line3Slope;
-                setPixel(screenData, (int)line1X, y, 0x7F, 0xFF, 0);
-                setPixel(screenData, (int)line3X, y, 0x7F, 0xFF, 0);
                 if (line1X < line3X) {
                     for (int x = line1X; x < line3X; x++) {
                         setPixel(screenData, x, y, 0x7F, 0xFF, 0);
@@ -65,6 +73,8 @@ void renderFrame(u8* screenData) {
                         setPixel(screenData, x, y, 0x7F, 0xFF, 0);
                     }
                 }
+                line1X += line1Slope;
+                line3X += line3Slope;
             }
         }
     }
